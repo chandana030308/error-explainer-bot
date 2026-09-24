8312b685985826d06dcd05595d7768e5dc35a174
chandana030308 <chandana030308@gmail.com>
2026-09-24 11:23:50 +0530

    Added while loop to repeat

--- main.py
+++ main.py
@@ -9,16 +9,26 @@ you explain programming error messages to a 2nd year engineering student . reply
 3) how to fix 
 keep it under 120 words.
 '''
-response = ollama.chat(
-    model="gemma3:1b",messages=[
-        {
-            "role": "system",
-            "content": SYSTEM
-        },
-        {
-            "role": "user",
-            "content": "python error - NameError : name 'x' is not defined"
-        }
-    ]
-)
-print(response.message.content)
+while True:
+    data = input("enter your error message or exit to stop: ")
+    if not data:
+        print("enter an error:")
+    elif data.lower() == "exit":
+        break
+    elif data.lower() == "help":
+        print("enter the error message you want to understand or type 'exit'")
+    else:
+        try: 
+            response = ollama.chat(model="gemma3:1b",messages=[
+            {
+                "role": "system",
+                "content": SYSTEM
+            },
+            {
+                "role": "user",
+                "content": "python error - NameError : name 'x' is not defined"
+            }
+            ])
+            print(response.message.content)
+        except Exception as e:
+            print(f"could not reach the model: {e}")
